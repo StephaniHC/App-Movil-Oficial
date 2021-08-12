2122af3e79a6e5f97ef0d288286d97f5f2cea325
Franklin <franklin.ojc@gmail.com>
2021-08-12 17:09:55 -0400

    #HU- implementar componente mapa (mostrar marcadores de inicio y destino) #dev-franklin

--- lib/bloc/mapa/mapa_bloc.dart
+++ lib/bloc/mapa/mapa_bloc.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-
+import 'package:app_movil_oficial/helpers/helpers.dart';
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart' show Colors;
+import 'package:flutter/material.dart' show Colors, Offset;
 import 'package:meta/meta.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,126 +11,142 @@ part 'mapa_event.dart';
 part 'mapa_state.dart';
 
 class MapaBloc extends Bloc<MapaEvent, MapaState> {
-
-  MapaBloc() : super( new MapaState() );
+  MapaBloc() : super(new MapaState());
 
   // Controlador del mapa
   GoogleMapController _mapController;
 
   // Polylines
   Polyline _miRuta = new Polyline(
-    polylineId: PolylineId('mi_ruta'),
-    width: 4,
-    color: Colors.transparent
-  );
+      polylineId: PolylineId('mi_ruta'), width: 4, color: Colors.transparent);
 
   //polyline de ruta destino
-   Polyline _miRutaDestino = new Polyline(
+  Polyline _miRutaDestino = new Polyline(
     polylineId: PolylineId('_miRutaDestino'),
     width: 4,
     color: Colors.green,
   );
 
-  void initMapa( GoogleMapController controller ) {
-    if ( !state.mapaListo ) {
+  void initMapa(GoogleMapController controller) {
+    if (!state.mapaListo) {
       this._mapController = controller;
 
-      add( OnMapaListo() );
+      add(OnMapaListo());
     }
   }
 
-  void moverCamara( LatLng destino ) {
+  void moverCamara(LatLng destino) {
     final cameraUpdate = CameraUpdate.newLatLng(destino);
     this._mapController?.animateCamera(cameraUpdate);
   }
 
-
   @override
-  Stream<MapaState> mapEventToState( MapaEvent event ) async* {
-    
-    if ( event is OnMapaListo ) {
-      yield state.copyWith( mapaListo: true );
-
-    } else if ( event is OnNuevaUbicacion ) {
-      yield* this._onNuevaUbicacion( event );
-
-    } else if ( event is OnMarcarRecorrido ) {
-      yield* this._onMarcarRecorrido( event );
-
-    } else if ( event is OnSeguirUbicacion ) {
-      yield* this._onSeguirUbicacion( event );
-
-    } else if ( event is OnMovioMapa ) {
+  Stream<MapaState> mapEventToState(MapaEvent event) async* {
+    if (event is OnMapaListo) {
+      yield state.copyWith(mapaListo: true);
+    } else if (event is OnNuevaUbicacion) {
+      yield* this._onNuevaUbicacion(event);
+    } else if (event is OnMarcarRecorrido) {
+      yield* this._onMarcarRecorrido(event);
+    } else if (event is OnSeguirUbicacion) {
+      yield* this._onSeguirUbicacion(event);
+    } else if (event is OnMovioMapa) {
       //print( event.centroMapa );
-      yield state.copyWith( ubicacionCentral: event.centroMapa );
-
-    } else if( event is OnCrearRutaInicioDestino){
-        yield* this._onCrearRutaInicioDestino(event);
+      yield state.copyWith(ubicacionCentral: event.centroMapa);
+    } else if (event is OnCrearRutaInicioDestino) {
+      yield* this._onCrearRutaInicioDestino(event);
     }
-
   }
 
-  
-  Stream<MapaState> _onNuevaUbicacion( OnNuevaUbicacion event ) async* {
-
-    if ( state.seguirUbicacion ) {
-      this.moverCamara( event.ubicacion );
+  Stream<MapaState> _onNuevaUbicacion(OnNuevaUbicacion event) async* {
+    if (state.seguirUbicacion) {
+      this.moverCamara(event.ubicacion);
     }
 
-
-    final points = [ ...this._miRuta.points, event.ubicacion ];
-    this._miRuta = this._miRuta.copyWith( pointsParam: points );
+    final points = [...this._miRuta.points, event.ubicacion];
+    this._miRuta = this._miRuta.copyWith(pointsParam: points);
 
     final currentPolylines = state.polylines;
     currentPolylines['mi_ruta'] = this._miRuta;
 
-    yield state.copyWith( polylines: currentPolylines );
-
+    yield state.copyWith(polylines: currentPolylines);
   }
 
-  Stream<MapaState> _onMarcarRecorrido( OnMarcarRecorrido event ) async* {
-
-    if ( !state.dibujarRecorrido ) {
-      this._miRuta = this._miRuta.copyWith( colorParam: Colors.black87 );
+  Stream<MapaState> _onMarcarRecorrido(OnMarcarRecorrido event) async* {
+    if (!state.dibujarRecorrido) {
+      this._miRuta = this._miRuta.copyWith(colorParam: Colors.black87);
     } else {
-      this._miRuta = this._miRuta.copyWith( colorParam: Colors.transparent );
+      this._miRuta = this._miRuta.copyWith(colorParam: Colors.transparent);
     }
 
     final currentPolylines = state.polylines;
     currentPolylines['mi_ruta'] = this._miRuta;
 
-    yield state.copyWith( 
-      dibujarRecorrido: !state.dibujarRecorrido,
-      polylines: currentPolylines
-    );
-
+    yield state.copyWith(
+        dibujarRecorrido: !state.dibujarRecorrido, polylines: currentPolylines);
   }
 
-  Stream<MapaState> _onSeguirUbicacion( OnSeguirUbicacion event ) async* {
-
-    if ( !state.seguirUbicacion ) {
-      this.moverCamara( this._miRuta.points[ this._miRuta.points.length - 1 ] );
+  Stream<MapaState> _onSeguirUbicacion(OnSeguirUbicacion event) async* {
+    if (!state.seguirUbicacion) {
+      this.moverCamara(this._miRuta.points[this._miRuta.points.length - 1]);
     }
-    yield state.copyWith( seguirUbicacion: !state.seguirUbicacion );
+    yield state.copyWith(seguirUbicacion: !state.seguirUbicacion);
   }
 
-   Stream<MapaState> _onCrearRutaInicioDestino(OnCrearRutaInicioDestino event ) async* {
-
-
-     this._miRutaDestino = this._miRutaDestino.copyWith(
-      pointsParam: event.rutaCoordenadas
-    );
+  Stream<MapaState> _onCrearRutaInicioDestino(
+      OnCrearRutaInicioDestino event) async* {
+    this._miRutaDestino =
+        this._miRutaDestino.copyWith(pointsParam: event.rutaCoordenadas);
 
     final currentPolylines = state.polylines;
     currentPolylines['mi_ruta_destino'] = this._miRutaDestino;
 
+    //Marcadores
+    double kilometros = event.distancia / 1000;
+    kilometros = (kilometros * 100).floor().toDouble();
+    kilometros = kilometros / 100;
+
+    // Icono destino
+    final iconoInicio  = await getAssetImageMarker('inicio');
+    final iconoDestino  = await getAssetImageMarker('destino');
+    
+    //marcador inicio
+    final markerInicio = new Marker(
+      icon: iconoInicio,
+       anchor: Offset(0.0, 1.0),
+       markerId: MarkerId('inicio'),
+       position: event.rutaCoordenadas[0],
+       infoWindow: InfoWindow(
+        title: 'Mi Ubicación',
+        snippet: 'Duración recorrido: ${ (event.duracion / 60).floor() } minutos',
+       )
+    );
+
+    final markerDestino = new Marker(
+      icon: iconoDestino,
+      markerId: MarkerId('destino'),
+      position: event.rutaCoordenadas[ event.rutaCoordenadas.length - 1],
+      anchor: Offset(0.1, 0.90),
+      infoWindow: InfoWindow(
+        title: event.nombreDestino,
+        snippet: 'Distancia: $kilometros Km',
+      )
+      );  
+
+       final newMarkers = { ...state.markers };
+        newMarkers['inicio']   = markerInicio;
+        newMarkers['destino']  = markerDestino;
+
+        Future.delayed(Duration(milliseconds: 300)).then(
+      (value) {
+         //_mapController.showMarkerInfoWindow(MarkerId('inicio'));
+         _mapController.showMarkerInfoWindow(MarkerId('destino'));
+      }
+    );
 
     yield state.copyWith(
       polylines: currentPolylines,
-      // TODO: Marcadores
+      markers: newMarkers
     );
-
-
-   }
+  }
 }
-
--- lib/bloc/mapa/mapa_event.dart
+++ lib/bloc/mapa/mapa_event.dart
@@ -3,30 +3,27 @@ part of 'mapa_bloc.dart';
 @immutable
 abstract class MapaEvent {}
 
-class OnMapaListo extends MapaEvent{}
+class OnMapaListo extends MapaEvent {}
 
-class OnMarcarRecorrido extends MapaEvent{}
+class OnMarcarRecorrido extends MapaEvent {}
 
-class OnSeguirUbicacion extends MapaEvent{}
+class OnSeguirUbicacion extends MapaEvent {}
 
-class OnCrearRutaInicioDestino extends MapaEvent{
-
+class OnCrearRutaInicioDestino extends MapaEvent {
   final List<LatLng> rutaCoordenadas;
   final double distancia;
   final double duracion;
+  final String nombreDestino;
 
-  OnCrearRutaInicioDestino( this.rutaCoordenadas, this.distancia, this.duracion );
-
+  OnCrearRutaInicioDestino(this.rutaCoordenadas, this.distancia, this.duracion, this.nombreDestino);
 }
 
-class OnMovioMapa extends MapaEvent{
+class OnMovioMapa extends MapaEvent {
   final LatLng centroMapa;
   OnMovioMapa(this.centroMapa);
-  
 }
 
-class OnNuevaUbicacion extends MapaEvent{
+class OnNuevaUbicacion extends MapaEvent {
   final LatLng ubicacion;
   OnNuevaUbicacion(this.ubicacion);
-
 }
